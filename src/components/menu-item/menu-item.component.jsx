f5eb3a0c0c6eee0d203409abd0606d467ab69623
Michael Tower PC <michaeljyt@gmail.com>
2020-04-12 11:39:10 +0200

    update menu-items to navigate withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { withRouter} from 'react-router-dom'
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size, id, linkUrl}) => {
+// mit dem Export unten, gibt es jetzt access zum history objekt!
+const MenuItem = ({title, imageUrl, size, id, linkUrl, history, match}) => {
 
     return (
-        <div className={`${size} menu-item`} >
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div 
                 className='background-image' 
                 style={{
@@ -20,4 +21,5 @@ const MenuItem = ({title, imageUrl, size, id, linkUrl}) => {
     )
 }
 
+// Das Teil returned ein Super powered MenuItem
 export default withRouter(MenuItem)
