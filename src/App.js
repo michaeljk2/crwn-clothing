cba4b3855733f8a2b081dd7bc753c03418a2e57b
Michae Payne <michaeljyt@gmail.com>
2020-04-13 19:48:59 +0200

    Lesson 72 Complete

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Switch, Route, Link } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-      </Switch>
+      <Header />
+      <div className='mainBody'>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crwn.svg";
+
+import "./header.styles.scss";
+
+const Header = (props) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+    </Link>
+    <div className='options'>
+        <Link className='option' to='/shop'>
+            shop
+        </Link>
+        <Link className='option' to='/contact'>
+            contact
+        </Link>
+    </div>
+  </div>
+)
+
+export default Header
